1ef452d0e04638ed0f3f4483d90cf9178c4cabef
tyh497020778 <tyh497020778@163.com>
2019-12-30 09:46:44 +0800

    指定文件目录的路径

--- jiaxiangwei_back/src/main/java/com/shuanghui/jiaxiangwei/StartupApplication.java
+++ jiaxiangwei_back/src/main/java/com/shuanghui/jiaxiangwei/StartupApplication.java
@@ -2,7 +2,6 @@ package com.shuanghui.jiaxiangwei;
 
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.Banner;
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
@@ -20,8 +19,4 @@ public class StartupApplication extends SpringBootServletInitializer{
     private static  SpringApplicationBuilder configureApplication(SpringApplicationBuilder builder){
         return builder.sources(StartupApplication.class).bannerMode(Banner.Mode.OFF);
     }
-
-//    public static  void  main(String[] args){
-//        SpringApplication.run(StartupApplication.class,args);
-//    }
 }
